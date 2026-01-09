a41363a3e111e5d40fba5e96fc490bc0e732b2c6
Chavanon-bit <ryu.opaspakkthon@gmail.com>
2026-01-09 13:29:18 +0700

    Update Ex_1_A_6788202.py

--- Python_work/L1/Ex_1_A_6788202.py
+++ Python_work/L1/Ex_1_A_6788202.py
@@ -7,27 +7,21 @@ Date of work: January 9, 2026
 """
 
 # TASK 1.1
-#Your_code_here
 a = 10
 b = 3
 print(a + b)
 # TASK 1.2
-#Your_code_here
 print(a - b)
 # TASK 1.3
-#Your_code_here
 print(a * b)
 # TASK 1.4
-#Your_code_here
 print(a / b)
 # TASK 1.5
-#Your_code_here
 print(int(a / b))
 # TASK 1.6
-#Your_code_here
 print(a % b)
 # TASK 1.7
-#Your_code_here
 print(a ** b)
+
 
 
