eb69970066e96fe87979609d9bb1667ac6dcd4cb
Chavanon-bit <ryu.opaspakkthon@gmail.com>
2026-01-09 13:30:04 +0700

    Update Ex_1_E_6788202.py

--- Python_work/L1/Ex_1_E_6788202.py
+++ Python_work/L1/Ex_1_E_6788202.py
@@ -7,13 +7,13 @@ Date of work: January 9, 2026
 """
 
 # TASK 1.1
-#Your_code_here
 mylist = [55,78,82,90,67]
 mylist.append(88)
 mylist.remove(55)
 print(len(mylist))
 for x in mylist:
   print(x)
+
 
 
 
