f3062b50bb642242043d7066a80078a4556917a8
Chavanon-bit <ryu.opaspakkthon@gmail.com>
2026-01-09 13:29:34 +0700

    Update Ex_1_B_6788202.py

--- Python_work/L1/Ex_1_B_6788202.py
+++ Python_work/L1/Ex_1_B_6788202.py
@@ -7,11 +7,11 @@ Date of work: January 9, 2026
 """
 
 # TASK 1.1
-#Your_code_here
 x = input("Enter your name: ")
 y = int(input("Enter your age: "))
 y += 1
 print("Hello " + x + ",next year you will be " +  str(y) + ".")
+
 
 
 
